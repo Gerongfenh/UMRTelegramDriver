4c4af93c7b4f94bd78a42bad2bf3dfc95870ef0c
Curtis Jiang <jqqqqqqqqqq@gmail.com>
2020-03-21 02:06:03 -0400

    initial commit

---
+++ setup.py
@@ -0,0 +1,44 @@
+import sys
+from setuptools import setup, find_packages
+import umr_telegram_driver
+
+if sys.version_info < (3, 7):
+    raise Exception("Python 3.7 or higher is required. Your version is %s." % sys.version)
+
+long_description = open('README.md', encoding="utf-8").read()
+
+__version__ = umr_telegram_driver.__VERSION__
+
+setup(
+    name='umr_telegram_driver',
+    packages=find_packages(include=['umr_telegram_driver', 'umr_telegram_driver.*']),
+    version=__version__,
+    description='UMR Telegram Driver',
+    long_description=long_description,
+    author='Curtis Jiang',
+    url='https://github.com/jqqqqqqqqqq/UnifiedMessageRelay',
+    license='MIT',
+    python_requires='>=3.7',
+    include_package_data=True,
+    zip_safe=False,
+    keywords=['UMR', 'UnifiedMessageRelay', 'Group chat relay', 'IM', 'messaging', 'Telegram'],
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "License :: OSI Approved :: MIT License",
+        "Intended Audience :: Developers",
+        "Intended Audience :: End Users/Desktop",
+        "Programming Language :: Python :: 3 :: Only",
+        "Programming Language :: Python :: 3.7",
+        "Topic :: Communications :: Chat",
+        "Topic :: Software Development :: Libraries :: Application Frameworks",
+        "Typing :: Typed"
+    ],
+    install_requires=[
+        'aiogram',
+        'unified-message-relay',
+        'janus'
+    ],
+    project_urls={
+        "Telegram Chat": "https://t.me/s/UnifiedMessageRelayDev",
+    }
+)
