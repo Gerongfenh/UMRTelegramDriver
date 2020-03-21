7a724444ea918050c90ff9b099f5f3d00314b01d
Curtis Jiang <jqqqqqqqqqq@gmail.com>
2020-03-21 02:16:21 -0400

    fix import

--- setup.py
+++ setup.py
@@ -1,18 +1,16 @@
 import sys
 from setuptools import setup, find_packages
-import umr_telegram_driver
+from umr_telegram_driver.__VERSION__ import __VERSION__
 
 if sys.version_info < (3, 7):
     raise Exception("Python 3.7 or higher is required. Your version is %s." % sys.version)
 
 long_description = open('README.md', encoding="utf-8").read()
 
-__version__ = umr_telegram_driver.__VERSION__
-
 setup(
     name='umr_telegram_driver',
     packages=find_packages(include=['umr_telegram_driver', 'umr_telegram_driver.*']),
-    version=__version__,
+    version=__VERSION__,
     description='UMR Telegram Driver',
     long_description=long_description,
     author='Curtis Jiang',
